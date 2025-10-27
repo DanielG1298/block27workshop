653887c30d00145aad9358bbe6cb29c8bbe5d9e2
DanielG1298 <glaser.daniel1204@gmail.com>
2025-10-27 18:24:02 -0500

    add app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+import GuestDetails from "./guests/guestDetails.jsx";
+import { GuestLists } from "./guests/guestLists.jsx";
+
 export default function App() {
-  return <></>;
+  const [GuestId, setGuestId] = useState();
+  return <>
+    <main>
+      {GuestId ? (
+        <GuestDetails GuestId={GuestId} setGuestId={setGuestId}/>
+      ) : (
+        <GuestLists setGuestId={setGuestId} guestId={GuestId}/>
+      
+      )}
+    </main>
+    
+  </>;
 }
